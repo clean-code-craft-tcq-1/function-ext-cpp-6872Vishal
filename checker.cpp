da00006f77aa06877eeff018b98546137a17ad94
6872Vishal <77484050+6872Vishal@users.noreply.github.com>
2021-03-09 17:27:58 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -2,31 +2,23 @@
 #include <iostream>
 using namespace std;
 
-#define Temperature_Min  0
-#define Temperature_Max  45
-#define SOC_Min  20
-#define SOC_Max  80
-#define ChargeRate_Min  0.0
-#define ChargeRate_Max  0.8
 /* BMS : Battery Management System */
 enum BMS_Parameters {Temperature,Sate_of_Charge,Charge_Rate};
 enum BMS_Lang {English,German};
 const char* BMS_Parameters_Name[] = { "Temperature" ,"SOC" , "ChargeRate" };
 
 const char* BMS_Lang_Names[][3]={ {"Temperature" ,"SOC" , "ChargeRate"},{"Temperatur" , "Ladezustand" , "Ladestrom"} };
-// enum BMS_Parameters_Min_Value {Temperature_Min = 0,  SOC_Min = 20, ChargeRate_Min = 0.0};
-// enum BMS_Parameters_Max_Value {Temperature_Max = 45, SOC_Max = 80, ChargeRate_Max = 0.8};
+
 struct BMS_Parameters_MAX_and_MIN_Values
 {
   enum BMS_Parameters Parameter_Name;
   float Minimum_Value;
   float Maximum_Value;
-  const char* BMS_English_Name[];
 };
 
-struct BMS_Parameters_MAX_and_MIN_Values BMS_Parameter_MaxMin_st[3]={ {Temperature, (float)0 , (float)45 , "Temperature"}, 
-                                                                      {Sate_of_Charge, (float)20 , (float)80 , "SOC"}, 
-                                                                      {Charge_Rate, (float)0.0 , (float)0.8 , "ChargeRate"} 
+struct BMS_Parameters_MAX_and_MIN_Values BMS_Parameter_MaxMin_st[3]={ {Temperature, (float)0 , (float)45 }, 
+                                                                      {Sate_of_Charge, (float)20 , (float)80 }, 
+                                                                      {Charge_Rate, (float)0.0 , (float)0.8 } 
                                                                     };
 
 class BMS{
@@ -55,7 +47,7 @@ class BMS{
       }
       else
       {
-        cout<<" "<<BMS_Lang_Names[Language][name]<<" is Normal \n";
+        cout<<BMS_Lang_Names[Language][name]<<" is Normal \n";
       }
     }
     /* Verifies the given Parameter is within limits or not */
@@ -63,9 +55,10 @@ class BMS{
     {
       float min = BMS_Parameter_MaxMin_st[name].Minimum_Value;
       float max = BMS_Parameter_MaxMin_st[name].Maximum_Value;
+      const char* Parameter_Name = BMS_Lang_Names[Language][name];
       if(parameter < min)
       {
-        cout <<"Low "<<BMS_Lang_Names[Language][name]<<" Breach! \n";
+        cout <<"Low "<<Parameter_Name<<" Breach! \n";
         BMS_OK = false;
       }
       else if(parameter > max)
