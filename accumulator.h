3c647bca25cb200b8b9c1a4a3df6374c3466cd1c
6872Vishal <77484050+6872Vishal@users.noreply.github.com>
2021-03-17 00:10:08 +0530

    Update accumulator.h

--- accumulator.h
+++ accumulator.h
@@ -5,16 +5,16 @@ protected:
 std::string accumulator_str;
 //float Parameters_reported_value[Max_NoOf_BMS_Parameters];
 public:
-virtual void accumulator(float ,BMS_Parameters ,std::string ) = 0;
+virtual void accumulator(float ,BMS_Parameters ,std::string ,std::string) = 0;
 virtual void reporting() = 0;
 };
 
 class BMS_Car_System:public base
 {
 public:
-void accumulator(float parameter,BMS_Parameters name_en,std::string print_text)
+void accumulator(float parameter,BMS_Parameters name_en,std::string para_text,std::string breach_text)
 {
-  accumulator_str = accumulator_str + "\n" + print_text;
+  accumulator_str = accumulator_str + "\n" + para_text + breach_text;
 }
 void reporting()
 {
